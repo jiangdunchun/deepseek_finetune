833973ddf1fba051bb7446ef173d64a7d8ce2684
JiangDunchun <JiangDunchun@163.com>
2025-06-21 15:52:57 +0800

    add train_data.py

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ pip install swanlab[dashboard]
 
 https://github.com/CherryHQ/cherry-studio
 
-python proccess_data.py --conversation=./train_data/conversation1.md --train_data=./train_data/train_data.jsonl
-
-torchrun finetune.py --model_name_or_path=./.cache/modelscope/models/deepseek-ai/deepseek-llm-7b-chat/ --train_data=./train_data/train_data.jsonl
-
 git lfs install
 cd ./output
 git clone https://oauth2:1EDrxJLfQrAcLsadR8yT@www.modelscope.cn/JiangDunchun/deepseek-7b-finetune.git
+
+python train_data.py --prompt=./train_data/prompt.txt --question=./train_data/question.txt --topic=./train_data/vup_mcp.md --output=./train_data/train_data.jsonl
+
+torchrun finetune.py --model_name_or_path=./.cache/modelscope/models/deepseek-ai/deepseek-llm-7b-chat/ --train_data=./train_data/train_data.jsonl
 
 python merge_lora.py --pretrained=./.cache/modelscope/models/deepseek-ai/deepseek-llm-7b-chat --finetuned=./output --merged=./output/deepseek-7b-finetune
 
---
+++ train_data.py
@@ -0,0 +1,152 @@
+import argparse
+from dotenv import load_dotenv
+import os
+import requests
+import time
+import json
+from typing import List
+from dataclasses import dataclass
+
+
+@dataclass
+class Round:
+    user:str
+    assistant:str
+@dataclass
+class Conversation:
+    conversation_id:int
+    conversation:List[Round]
+def dataclass_to_dict(instance):
+    if hasattr(instance, '__dataclass_fields__'):
+        return {
+            field.name: dataclass_to_dict(getattr(instance, field.name))
+            for field in instance.__dataclass_fields__.values()
+        }
+    elif isinstance(instance, list):
+        return [dataclass_to_dict(item) for item in instance]
+    return instance
+
+
+def init_args():
+    parser = argparse.ArgumentParser(description="Prepare the train data")
+
+    parser.add_argument("--prompt", type=str, default="",
+                        help="prompt text file")
+    parser.add_argument("--question", type=str, default="",
+                        help="question file in lines")
+    parser.add_argument("--topic", type=str, default="",
+                        help="topic conversation exported from Cherry Studio")
+    parser.add_argument("--auth_key", type=str, default="",
+                        help="auth key of deekseek, also can be set in .env by DEEPSEEK_AUTH_KEY=**********")
+    parser.add_argument("--output", type=str, required=True,
+                        help="train data in jsonl format")
+
+    args = parser.parse_args()
+
+    args.prompt_txt = ""
+    if args.prompt != "":
+        with open(args.prompt, 'r', encoding='utf-8') as file:
+            args.prompt_txt = file.read()
+
+    load_dotenv()
+    if args.auth_key == "":
+        args.auth_key = os.getenv("DEEPSEEK_AUTH_KEY", "")
+
+    return args
+
+
+def ask_deepseek(args):
+    if args.question == "": 
+        return None
+
+    url = "https://api.deepseek.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {args.auth_key}"
+    }
+
+    data = {
+        "model": "deepseek-chat",
+        "messages": [],
+        "stream": False
+    }
+
+    if args.prompt_txt != "":
+        data["messages"].append({
+            "role": "system",
+            "content": args.prompt_txt
+        })
+
+    rounds = [] 
+
+    with open(args.question, 'r', encoding='utf-8') as file: 
+        for line in file:
+            ask = line.strip()
+            if ask == '' or ask == '---': continue
+
+            data["messages"].append({
+                "role": "user",
+                "content": ask
+            })
+
+            response = requests.post(url, headers=headers, json=data)
+
+            if response.status_code == 200:
+                result = response.json()
+                answer = result["choices"][0]["message"]["content"]
+                data["messages"].append({
+                    "role": "assistant",
+                    "content": ask
+                })
+                print("user>>>>>>>>>>>>>>>>>>>>>>\n", ask)
+                print("assistant>>>>>>>>>>>>>>>>>\n", answer)
+                rounds.append(Round(user=ask, assistant=answer))
+        
+    conversation = Conversation(conversation_id=int(time.time()), conversation=rounds)
+    return conversation
+
+def process_topic(args):
+    if args.topic == "": 
+        return None
+
+    with open(args.topic, 'r', encoding='utf-8') as file: 
+        contents, role, content = [], '', ''
+        for line in file:
+            line = line.strip()
+            if line == '' or line == '---': continue
+            if line.startswith('###'):
+                if role != '' and content != '': contents.append([role, content])
+
+                words = line.split(' ')
+                role = words[-1]
+                content = ''
+            else:
+                if content != '': content += '\\n'
+                content += line
+        if role != '' and content != '': contents.append([role, content])
+
+    
+    num_contents, rounds = len(contents) // 2 * 2, [] 
+    for i in range(num_contents//2):
+        role0, content0, role1, content1 = contents[2*i][0], contents[2*i][1], contents[2*i+1][0], contents[2*i+1][1]
+        if role0 == 'User' and role1 == 'Assistant':
+            rounds.append(Round(user=content0, assistant=content1))
+    
+    conversation = Conversation(conversation_id=int(time.time()), conversation=rounds)
+    return conversation
+
+
+def append_conversation(args, conversation):
+    with open(args.output, 'a', encoding='utf-8') as file:
+        file.write(json.dumps(dataclass_to_dict(conversation)))
+        file.write('\n')
+
+
+if __name__ == "__main__":
+    args = init_args()
+    conversation = ask_deepseek(args)
+    if conversation != None:
+        append_conversation(args, conversation)
+    conversation = process_topic(args)
+    if conversation != None:
+        append_conversation(args, conversation)
